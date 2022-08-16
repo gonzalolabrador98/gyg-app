6273370553c94e442c28fac14330ac33098f7f18
unknown <gonzalolabrador98@gmail.com>
2022-08-15 22:33:35 -0300

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { Navbar } from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="App">
+        <h1>G&G APP</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
